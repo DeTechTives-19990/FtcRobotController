ff4f9f76b87e2ad1cc9cbe2713100254503b0dcd
chayk0926-prog <chayk0926@gmail.com>
2025-11-27 16:51:22 -0600

    Add alternative distance calculation as comments
    Commented out alternative distance calculation using NetworkTable.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deTech/limelightDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deTech/limelightDistance.java
@@ -14,5 +14,25 @@ public class LimelightDistance {
         // Distance in SAME UNITS as heights above
         double distance = (TARGET_HEIGHT - LIMELIGHT_HEIGHT) / Math.tan(angleRad);
         return distance;
+
+        //try this if it doenst work:
+    //     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    // NetworkTableEntry ty = table.getEntry("ty");
+    // double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+
+    // // how many degrees back is your limelight rotated from perfectly vertical?
+    // double limelightMountAngleDegrees = 25.0; 
+
+    // // distance from the center of the Limelight lens to the floor
+    // double limelightLensHeightInches = 20.0; 
+
+    // // distance from the target to the floor
+    // double goalHeightInches = 60.0; 
+
+    // double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    // double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+    // //calculate distance
+    // double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians)
     }
 }
